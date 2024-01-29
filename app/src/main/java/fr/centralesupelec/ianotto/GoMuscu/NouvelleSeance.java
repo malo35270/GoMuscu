0b05f2540dd512f438947d49695a460190c8d018
ethan <ethan.charbonnel@student-cs.fr>
2024-01-29 23:43:09 +0100

    added music

--- app/src/main/java/fr/centralesupelec/ianotto/GoMuscu/NouvelleSeance.java
+++ app/src/main/java/fr/centralesupelec/ianotto/GoMuscu/NouvelleSeance.java
@@ -1,40 +1,89 @@
 package fr.centralesupelec.ianotto.GoMuscu;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
-public class NouvelleSeance extends AppCompatActivity implements View.OnClickListener {
-    private Button boutonSeanceA;
-    private Button boutonSeanceB;
-    private Button boutonSeanceC;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Vector;
+
+public class NouvelleSeance extends AppCompatActivity {
+    private Vector<Button> listSeances;
     private ContactOperations contactOperations;
 
+    private LinearLayout linear;
+    //for JSON reading :
+    private JSONHandler handler = new JSONHandler();
+    int nb;
+    JSONArray arr;
 
+    @SuppressLint({"ResourceAsColor", "UseCompatLoadingForDrawables"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nouvelle_seance);
 
-        // On récupère une référence sur le bouton Effacer
-        boutonSeanceA = findViewById(R.id.boutonSeanceA);
-        boutonSeanceA.setText("Pecs / Biceps");
+        linear = findViewById(R.id.linear);
 
-        boutonSeanceB = findViewById(R.id.boutonSeanceB);
-        boutonSeanceB.setText("Dos / Triceps");
+        try {
+            String file = "jsonfileCurrent.json";
+            JSONObject obj = new JSONObject(handler.LectureJSON(this, file));
+            Log.i("json", String.valueOf(obj.getInt("nb_de_seance")));
+            nb = obj.getInt("nb_de_seance");
+            for (int i = 1; i < nb + 1; i++) {
+                //params
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT
+                );
+                params.setMargins(60,20,60,20);
 
-        boutonSeanceC = findViewById(R.id.boutonSeanceC);
-        boutonSeanceC.setText("Jambes / Épaules");
+                Button myButton = new Button(this);
+                myButton.setLayoutParams(params);
+                myButton.setText(handler.getJsonSeance(obj, i));
+                myButton.setBackground(myButton.getContext().getResources().getDrawable(R.drawable.button));
+                myButton.setTextColor(myButton.getContext().getResources().getColor(R.color.black));
+                myButton.setId(i);
+
+                final int id_ = myButton.getId();
+                linear.addView(myButton);
+
+                myButton.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent i = new Intent();
+                        Button transform = (Button) v;
+
+                        //start activity and passes the name of the activity as an id
+                        i.setClass(getApplicationContext(), Seance.class);
+                        i.putExtra("seance", v.getId());
+                        startActivity(i);
+
+                    }
+                });
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        //handler.getJsonSeance(obj, 1);
+
         // Pour que la flèche s'affiche dans la barre de titre de l'activité
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Choisir la Séance");
         // On gère l'évènement "click" sur les boutons
-        boutonSeanceA.setOnClickListener(this);
-        boutonSeanceB.setOnClickListener(this);
-        boutonSeanceC.setOnClickListener(this);
-
     }
 
     // Méthode qui permet permet de revenir à l'activité précédente
@@ -46,14 +95,14 @@ public class NouvelleSeance extends AppCompatActivity implements View.OnClickLis
         return true;
     }
 
-    @Override
-    public void onClick(View v) {
-        Intent i = new Intent();
-        Button transform = (Button)v;
-
-        //start activity and passes the name of the activity as an id
-        i.setClass(getApplicationContext(), Seance.class);
-        i.putExtra("seance", transform.getText().toString());
-        startActivity(i);
-    }
+//    @Override
+//    public void onClick(View v) {
+//        Intent i = new Intent();
+//        Button transform = (Button)v;
+//
+//        //start activity and passes the name of the activity as an id
+//        i.setClass(getApplicationContext(), Seance.class);
+//        i.putExtra("seance", transform.getText().toString());
+//        startActivity(i);
+//    }
 }
--- app/src/main/java/fr/centralesupelec/ianotto/GoMuscu/Seance.java
+++ app/src/main/java/fr/centralesupelec/ianotto/GoMuscu/Seance.java
@@ -1,9 +1,14 @@
 package fr.centralesupelec.ianotto.GoMuscu;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.LiveFolders;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -12,39 +17,66 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Vector;
 
 public class Seance extends AppCompatActivity implements View.OnClickListener {
 
-    private CountDownTimer timer;
+
+    //Music
     private MediaPlayer mediaPlayer;
+    private Vector<String> musicList;
+    private int musicNum;
+    private TextView musicTitle;
+    private SeekBar musicBar;
+    private Context context;
+    private ImageButton next, prev, pause;
+
 
     //VIEW
-    private TextView name;
     private ListView list;
     private Button ser, rep, kg, plus1, plus2, plus3, old_rep, old_kg, old_ser, minus1, minus2, minus3, start, stop;
     private EditText minutes,seconds;
 
-    String seance, times, timem;
+    String seance;
+
+    //CHRONO :
+    private CountDownTimer timer;
+    private MediaPlayer alarm;
+    private String times, timem;
     private long timeRemainingInMillis;
-
-    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-    private ArrayList<String> listItems;
-
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    private ArrayAdapter<String> adapter;
     private boolean isRunning;
 
-    private JSONHandler file;
+
+    //LIST OF ARRAY STRINGS and StringAdapter for list d'exos
+    private ArrayList<String> listItems;
+    private ArrayAdapter<String> adapter;
+
+    //for JSON reading :
+    private String file = "jsonfileCurrent.json";
+    private JSONHandler handler = new JSONHandler();
+    int nb;
+    Vector<String> exo = new Vector<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,14 +86,31 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
         // Pour que la flèche s'affiche dans la barre de titre de l'activité
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        seance = getIntent().getExtras().getString("seance", "default_seance");
+        nb = getIntent().getExtras().getInt("seance", 0);
+
+        try {
+            String file = "jsonfileCurrent.json";
+            JSONObject obj = new JSONObject(handler.LectureJSON(this, file));
+            JSONArray arr = obj.getJSONArray("seance" + nb);
+            Log.i("json", String.valueOf(obj.getInt("nb_de_seance")));
+            Toast.makeText(this, "Loading Séance " + nb, Toast.LENGTH_LONG).show();
+            seance = handler.getJsonSeance(obj, nb);
+            Log.i("json", arr.toString());
+            for (int i = 1; i < arr.length(); i++) {
+                exo.add(handler.getJsonExo(arr, nb, i));
+            }
+        } catch (JSONException e) {
+            Toast.makeText(this, "error loading JSON", Toast.LENGTH_LONG).show();
+        }
+
+
+
+        //seance = getIntent().getExtras().getString("seance", "default_seance");
         //pour que le nom de la séance soit dans l'actionBar
         getSupportActionBar().setTitle(seance);
 
-
-
         //TAKES CARE OF THE LISTVIEW FOR EXERCICES
-        listItems = new ArrayList<String>(Arrays.asList("Chess Press", "Écarté haltère", "Traction supination", "Curl marteaux"));
+        listItems = new ArrayList<String>(exo);
         list = findViewById(R.id.listExo);
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
         list.setAdapter(adapter);
@@ -95,6 +144,13 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
         start = findViewById(R.id.start);
         stop = findViewById(R.id.stop);
 
+        next = findViewById(R.id.next);
+        prev = findViewById(R.id.prev);
+        pause = findViewById(R.id.pause);
+
+        musicTitle = findViewById(R.id.musicName);
+        musicBar = findViewById(R.id.musicBar);
+
         //set onclick view listener
         old_kg.setOnClickListener(this);
         old_rep.setOnClickListener(this);
@@ -115,6 +171,10 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
         start.setOnClickListener(this);
         stop.setOnClickListener(this);
 
+        prev.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        next.setOnClickListener(this);
+
         minutes = findViewById(R.id.minutes);
         minutes.setCursorVisible(false);
         minutes.setOnClickListener(this);
@@ -122,7 +182,58 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
         seconds.setCursorVisible(false);
         seconds.setOnClickListener(this);
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        //initiates audio
+        context = this;
+
+        alarm = MediaPlayer.create(this, R.raw.alarm);
+        musicList = listRaw();
+        musicNum = 1;
+        musicTitle.setText(musicList.get(musicNum));
+        int sound_id = context.getResources().getIdentifier(musicList.get(musicNum), "raw",
+                context.getPackageName());
+        if(sound_id != 0) {
+            mediaPlayer = MediaPlayer.create(context, sound_id);
+            mediaPlayer.start();
+        }
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        //Progress bar
+        musicBar.setMax(mediaPlayer.getDuration());
+        musicBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+//        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//            public void onCompletion(MediaPlayer mp) {
+//                mp.reset();
+//                musicNum = (musicNum + 1)%4;
+//                musicTitle.setText(musicList.get(musicNum));
+//                try {
+//                    mp.setDataSource(context, Uri.parse(musicList.get(musicNum)));
+//                } catch (IOException e) {
+//                    Toast.makeText(context, "sorry error", Toast.LENGTH_LONG).show();
+//                }
+//                mp.start();
+//            }
+//        });
+
+
+
+        //button
         stop.setEnabled(false);
 
         seconds.setOnKeyListener(new View.OnKeyListener() {
@@ -153,14 +264,6 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
                 return false;
             }
         });
-
-
-        JSONObject obj = new JSONObject();
-        String test = new String();
-        test = file.LectureJSON(this, "jsonfileCurrent.json");
-        Toast.makeText(this, test, Toast.LENGTH_LONG).show();
-
-
     }
 
     // Méthode qui permet permet de revenir à l'activité précédente
@@ -182,6 +285,27 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
         if (v == minus1) {ser.setText(String.valueOf(Integer.valueOf(ser.getText().toString()) - 1));}
         if (v == minus2) {rep.setText(String.valueOf(Integer.valueOf(rep.getText().toString()) - 1));}
         if (v == minus3) {kg.setText(String.valueOf(Integer.valueOf(kg.getText().toString()) - 1));}
+
+        if (v == prev) {
+            musicNum = 3;
+            String filename = "android.resource://" + this.getPackageName() + "/raw/" + musicList.get(musicNum);
+            try {
+                mediaPlayer.setDataSource(filename);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        if (v == next) {        if (v == prev) {
+            musicNum = ((musicNum - 1)%5);
+            if (musicNum==0){musicNum=4;}
+            String filename = "android.resource://" + this.getPackageName() + "/raw/" + musicList.get(musicNum);
+            try {
+                mediaPlayer.setDataSource(filename);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }}
+        if (v == pause) {if (mediaPlayer.isPlaying()) {mediaPlayer.stop();} else {mediaPlayer.start();}}
 
         //timer
         if (v == start) {
@@ -207,7 +331,8 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
                     }
                     @Override
                     public void onFinish() {
-                        mediaPlayer.start();
+                        alarm.start();
+                        alarmDialogBox();
                         timer.cancel();
                         timer = null;
                         isRunning=false;
@@ -236,5 +361,48 @@ public class Seance extends AppCompatActivity implements View.OnClickListener {
             minutes.setText(timem);
             seconds.setText(times);
         }
+    }
+
+
+
+    public void alarmDialogBox() {
+        // Create the object of AlertDialog Builder class
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        // Set Alert Title
+        builder.setTitle("ALARM");
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the button
+        builder.setNeutralButton("OK", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // If user click ok then dialog box is canceled.
+            dialog.cancel();
+            alarm.stop();
+        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+        // Show the Alert Dialog box
+        alertDialog.show();
+    }
+
+
+    public void play() {
+        int duration = mediaPlayer.getDuration();
+        int progress = 0;
+        musicBar.setProgress(progress);
+        musicTitle.setText(musicList.get(musicNum));
+    }
+
+    public Vector<String> listRaw(){
+        Vector<String> musicList = new Vector<>();
+        Field[] fields=R.raw.class.getFields();
+        for(int count=0; count < fields.length; count++){
+            musicList.add(fields[count].getName());
+            Log.i("Raw Asset: ", fields[count].getName());
+        }
+        return musicList;
     }
 }
